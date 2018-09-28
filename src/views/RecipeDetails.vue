ebb2ad8dd0fbc73ad190fab6793fc6c1608adcae
MikaelFeher <mikael.feher@gmail.com>
2018-09-28 21:37:31 +0200

    WIP - Adds page to view individual recipes.

---
+++ src/views/RecipeDetails.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="recipe-details" class="container row">
+    {{ recipe }}
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'RecipeDetails',
+  data() {
+    return {
+      recipe: {}
+    }
+  },
+  computed: {
+    ...mapState(['recipes'])
+  },
+  mounted: function() {
+    this.getRecipe()
+  },
+  methods: {
+    getRecipe() {
+      const { id } = this.$route.params;
+      console.log('id: ', id);
+      console.log('this.recipes: ', this.recipes);
+      const recipe = this.recipes.filter(recipe => recipe._id === id)
+      this.recipe = recipe[0]
+      console.log('this.recipe: ', this.recipe);
+    }
+  },
+
+}
+</script>
