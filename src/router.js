20b5a0c1af8fae5494106a45d37ea0c064bb450e
MikaelFeher <mikael.feher@gmail.com>
2018-10-02 11:45:26 +0200

    Removes Home1 page.

--- src/router.js
+++ src/router.js
@@ -1,13 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/Home.vue'
-import Home1 from '@/views/Home.1.vue'
 import RecipeDetails from '@/views/RecipeDetails.vue'
 import Admin from '@/views/admin/Admin.vue'
 import AddRecipe from '@/components/admin/AddRecipe.vue'
 import DeleteRecipe from '@/components/admin/DeleteRecipe.vue'
-
-
 
 Vue.use(Router)
 
@@ -15,11 +12,6 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/home1',
-      name: 'home1',
-      component: Home1, 
-    },
     {
       path: '/',
       name: 'home',
